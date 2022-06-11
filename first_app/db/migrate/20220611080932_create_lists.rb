e302131d013a05832fb695846d53b0c43e0ade2b
EC2 Default User <ec2-user@ip-172-31-16-77.ec2.internal>
2022-06-11 08:34:19 +0000

    [Finish]2-5

---
+++ first_app/db/migrate/20220611080932_create_lists.rb
@@ -0,0 +1,11 @@
+class CreateLists < ActiveRecord::Migration[6.1]
+  def change
+    create_table :lists do |t|
+      # 以下の2行を追加
+      t.string :title
+      t.string :body
+
+      t.timestamps
+    end
+  end
+end
